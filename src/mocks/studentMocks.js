9ff3e5eb9ea6d17e601691b4ad451f55a2b154af
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-05-20 13:49:12 +0300

    updated

--- src/mocks/studentMocks.js
+++ src/mocks/studentMocks.js
@@ -8,4 +8,4 @@ export const mockAbsences = [
   { date: '2024-06-09', time: '08:30', subject: 'Алгоритмизация и программирование', teacher: 'Петров П.П.', reason: 'Не указана', type: '', file: null, desc: '' },
   { date: '2024-06-05', time: '10:15', subject: 'Базы данных', teacher: 'Иванова А.А.', reason: 'Болезнь', type: 'illness', file: null, desc: '' },
   { date: '2024-06-03', time: '14:00', subject: 'Физика', teacher: 'Смирнов А.А.', reason: 'Уважительная', type: 'valid', file: null, desc: 'Семейные обстоятельства', accepted: true },
-]; 
+];
